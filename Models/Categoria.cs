cac20e1d00bde221cb0440bc254aa1bc1e0d38b4
shen <santi.dionis@gmail.com>
2023-09-11 21:58:10 +0200

    ADD:  DATAANNOTATIONS

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,8 +1,13 @@
 namespace proyectoef.Models;
+using System.ComponentModel.DataAnnotations;
 
 public class Categoria
 {
+    [key]
     public Guid CategoriaId { get; set; }
+
+    [Required(ErrorMessage = "El nombre es requerido")]
+    [MaxLength(150, ErrorMessage = "El nombre no puede tener mas de 150 caracteres")]
     public string Nombre { get; set; }
     public string Descripcion { get; set; }
 
--- Models/Tareas.cs
+++ Models/Tareas.cs
@@ -1,11 +1,17 @@
 namespace proyectoeft.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 {
     public class Tarea
     {
+        [Key]
         public Guid TareaId { get; set; }
 
+        [ForeigKey("CategoriaId")]
         public Guid CategoriaId { get; set; }
 
+        [Required(ErrorMessage = "El titulo es requerido")]
+        [MAXLength(200, ErrorMessage = "El titulo no puede tener mas de 200 caracteres")]
         public string Titulo {get; set;}
 
         public string Descripcion {get; set;}
