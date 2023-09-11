bf05f2d70bc47b7a7ab1854be13dc7e73867ad11
shen <santi.dionis@gmail.com>
2023-09-11 21:02:00 +0200

    ADD: Tareas

--- TareasContext.cs
+++ TareasContext.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Proyectoef.Models;
+
+
 namespace Proyectoef;
 
 public class TareasContesxt: DbContext{
 
-    
+    public Dbset<Categoria> Categorias {get; set;}
+    public Dbset<Tarea> Tareas {get; set;}
+
+    public TareasContesxt(DbContextOptions<TareasContesxt> options): base(options){}
+
+
 }
