df24c12f2134004300fca99d349e63a12472eb02
shen <santi.dionis@gmail.com>
2023-09-12 20:59:28 +0200

    ADD:  DATAANNOTATIONS

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+
+using proyectoeft;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
