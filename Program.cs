7db5dc66460c6e01ff29216168375a1495091799
shen <santi.dionis@gmail.com>
2023-09-14 20:54:25 +0200

    ADD SQL SERVER

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ using proyectoef;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+//builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+ //builder.Services.AddSqlServer<TareasContext>("Data Source=SHEN-PC\\SQLEXPRESS;Initial Catalog=TareasDB;user id SHEN-PC\\santi;password=almera;");
+ builder.Services.AddDbContext<TareasContext>(options => options.UseSqlServer("Data Source=(local); Initial Catalog= master;Trusted_Connection=True; Integrated Security=True"));
 
 var app = builder.Build();
 
